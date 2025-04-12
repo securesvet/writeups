636373194a042832c01ec38447aec6b21bbdda17
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-13 04:24:45 +0500

    fix: deleted top level await

--- src/pages/Writeups/index.tsx
+++ src/pages/Writeups/index.tsx
@@ -1,16 +1,20 @@
 import Markdown from "https://esm.sh/react-markdown@10";
 
-const markdown = await fetch("./docs/writeup/index.md").then((res) =>
-  res.text()
-);
+// const markdown = await fetch("./docs/writeup/index.md").then((res) =>
+//   res.text()
+// );
 
 function Writeups() {
   const writeups = [
     {
       name: "mursvet site",
       description: "mursvet site",
-    }
+    },
   ];
+
+  const markdown = `
+# Writeups
+`;
   return (
     <div className="grid grid-cols-2 gap-4 mt-[var(--header-height)]">
       <div>
@@ -19,14 +23,14 @@ function Writeups() {
       </div>
       <aside>
         <p>Writeups</p>
-      <div className="flex flex-col border-b-2">
-        {writeups.map((writeup) => (
-          <div>
-            <h3>{writeup.name}</h3>
-            <p>{writeup.description}</p>
-          </div>
-        ))}
-      </div>
+        <div className="flex flex-col border-b-2">
+          {writeups.map((writeup) => (
+            <div>
+              <h3>{writeup.name}</h3>
+              <p>{writeup.description}</p>
+            </div>
+          ))}
+        </div>
       </aside>
     </div>
   );
