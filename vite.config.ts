ac2133a39a1c8b0fa12b7aeba638872caaa82e30
Sviatoslav Murzin <mursvyats@gmail.com>
2025-04-15 03:12:55 +0500

    feat: obfuscating tailwind classes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import tailwindcss from "@tailwindcss/vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "npm:vite-plugin-svgr";
+import utwm from 'npm:unplugin-tailwindcss-mangle/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [deno(), react(), tailwindcss(), svgr()],
+  plugins: [deno(), react(), tailwindcss(), svgr(), utwm()],
   build: {
     outDir: "dist",
   },
